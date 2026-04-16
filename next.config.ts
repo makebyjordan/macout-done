ae92c10df4808f3674d65d456a4ca0f857d91b3a
makebyjordan <makebyjordan@gmail.com>
2026-04-16 02:05:28 +0200

    fix: update next.config.ts and stabilize nextjs version

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,21 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      handlebars: 'handlebars/dist/handlebars.js',
+    };
+    
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
